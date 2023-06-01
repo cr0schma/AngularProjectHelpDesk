1f5b9071ca3f84d1eaa6410cb578f499bb97d446
Chris Schmalz <cschmalz03@gmail.com>
2023-06-01 19:27:47 -0400

    init commit

---
+++ CreateHelpdeskDB.sql
@@ -0,0 +1,28 @@
+-- 1.
+Create Database Helpdesk
+GO
+
+USE [Helpdesk]
+
+-- 2.
+Create Table Tickets (
+	Id int IDENTITY(1,1) Primary Key,
+	Reporter nvarchar(50),
+	Assignee nvarchar(50),
+	Status nvarchar(6),
+	Title nvarchar(max),
+	Resolution nvarchar(max)
+);
+
+Create Table Favorites (
+	Id int IDENTITY(1,1),
+	Ticket_Number int
+);
+
+-- 3.
+Insert Into Tickets
+Values ('John Doe', 'Chris Schmalz', 'Closed', 'No Internet Connectivity', 'Plugged in network cable'),
+('Jane Doe', 'Chris Schmalz', 'Open', 'PC will not turn on', null);
+
+Insert Into Favorites
+Values ('1');
---
+++ README.md
@@ -0,0 +1,25 @@
+# HELP DESK TICKET SYSTEM
+
+A help desk ticketing system to assist users in the workplace
+
+## Authors
+
+- [@cr0schma](https://github.com/cr0schma)
+- 
+- 
+-
+
+## Project Structure
+
+```shell
+├── README.md
+├── Test
+│   ├── Test.txt
+│
+├── Test
+│   ├── Test
+```
+
+## App Operation
+
+* test
