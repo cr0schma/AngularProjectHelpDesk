1ee17c8f58caa578a72019c76902a9f4e478fe90
cr0schma <72145440+cr0schma@users.noreply.github.com>
2023-06-05 20:43:53 -0400

    replace contents

--- CreateHelpdeskDB.sql
+++ CreateHelpdeskDB.sql
@@ -1,8 +1,10 @@
+-- 1.
 Create Database Helpdesk
 GO
 
 USE [Helpdesk]
 
+-- 2.
 Create Table Tickets (
 	Id int IDENTITY(1,1) Primary Key,
 	Reporter nvarchar(50),
@@ -18,13 +20,10 @@ Create Table Favorites (
 	Ticket_Number int Foreign Key References Tickets(Id)
 );
 
-
+-- 3.
 Insert Into Tickets
 Values ('John Doe', 'Chris Schmalz', 'Closed', 'No Internet Connectivity', 'Plugged in network cable'),
 ('Jane Doe', 'Chris Schmalz', 'Open', 'PC will not turn on', null);
 
 Insert Into Favorites
-Values ('Chris Schmalz', 1);
-
---select * from Tickets;
---select * from Favorites;
+Values ('John Doe', 1);
