2ae9ee234dbf187eb6b0550e944a89e46c373de7
Gregory Wizner <wiznergregory@gmail.com>
2023-06-05 20:29:19 -0400

    SQL

--- CreateHelpdeskDB.sql
+++ CreateHelpdeskDB.sql
@@ -1,32 +1,30 @@
----- 1.
---Create Database Helpdesk
---GO
+Create Database Helpdesk
+GO
 
---USE [Helpdesk]
+USE [Helpdesk]
 
----- 2.
---Create Table Tickets (
---	Id int IDENTITY(1,1) Primary Key,
---	Reporter nvarchar(50),
---	Assignee nvarchar(50),
---	Status nvarchar(6),
---	Title nvarchar(max),
---	Resolution nvarchar(max)
---);
+Create Table Tickets (
+	Id int IDENTITY(1,1) Primary Key,
+	Reporter nvarchar(50),
+	Assignee nvarchar(50),
+	Status nvarchar(6),
+	Title nvarchar(max),
+	Resolution nvarchar(max)
+);
 
---Create Table Favorites (
---	Id int IDENTITY(1,1) Primary Key,
---	UserId nvarchar(50),
---	Ticket_Number int Foreign Key References Tickets(Id)
---);
+Create Table Favorites (
+	Id int IDENTITY(1,1) Primary Key,
+	UserId nvarchar(50),
+	Ticket_Number int Foreign Key References Tickets(Id)
+);
 
----- 3.
---Insert Into Tickets
---Values ('John Doe', 'Chris Schmalz', 'Closed', 'No Internet Connectivity', 'Plugged in network cable'),
---('Jane Doe', 'Chris Schmalz', 'Open', 'PC will not turn on', null);
 
---Insert Into Favorites
---Values ('Chris Schmalz', 1);
+Insert Into Tickets
+Values ('John Doe', 'Chris Schmalz', 'Closed', 'No Internet Connectivity', 'Plugged in network cable'),
+('Jane Doe', 'Chris Schmalz', 'Open', 'PC will not turn on', null);
+
+Insert Into Favorites
+Values ('Chris Schmalz', 1);
 
 --select * from Tickets;
 --select * from Favorites;
