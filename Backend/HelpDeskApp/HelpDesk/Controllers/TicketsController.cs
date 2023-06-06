a84948bd13decfe928fcba95947dfc1d1c4a1027
Gregory Wizner <wiznergregory@gmail.com>
2023-06-05 20:09:53 -0400

    removed bookmark

--- Backend/HelpDeskApp/HelpDesk/Controllers/TicketsController.cs
+++ Backend/HelpDeskApp/HelpDesk/Controllers/TicketsController.cs
@@ -28,27 +28,27 @@ namespace HelpDesk.Controllers
             return await _context.Tickets.ToListAsync();
         }
 
-        [HttpPost("Bookmark")]
-        public async Task<ActionResult<Favorite>> AddToFavorites(int id)
-        {
-            Ticket ticket = await _context.Tickets.FindAsync(id);
+        //[HttpPost("Bookmark")]
+        //public async Task<ActionResult<Favorite>> AddToFavorites(int id)
+        //{
+        //    Ticket ticket = await _context.Tickets.FindAsync(id);
 
-            if (ticket == null)
-            {
-                return NotFound();
-            }
+        //    if (ticket == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            Favorite favorite = new Favorite
-            {
-                TicketNumber = id,
-                TicketNumberNavigation = ticket
-            };
+        //    Favorite favorite = new Favorite
+        //    {
+        //        TicketNumber = id,
+        //        TicketNumberNavigation = ticket
+        //    };
 
-            _context.Favorites.Add(favorite);
-            _context.SaveChangesAsync();
+        //    _context.Favorites.Add(favorite);
+        //    _context.SaveChangesAsync();
 
-            return Ok();
-        }
+        //    return Ok();
+        //}
 
         // GET api/<TicketsController>/5
         //[HttpGet("{id}")]
