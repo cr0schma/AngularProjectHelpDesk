0dfdaa4c125e532cfdadb2501fead787f093f226
Gregory Wizner <wiznergregory@gmail.com>
2023-06-08 19:30:43 -0400

    css

--- Frontend/HelpDesk/src/app/app-routing.module.ts
+++ Frontend/HelpDesk/src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { ViewTicketsComponent } from './view-tickets/view-tickets.component';
 import { ViewFavoritesComponent } from './view-favorites/view-favorites.component';
 import { AddTicketComponent } from './add-ticket/add-ticket.component';
 import { UpdateTicketComponent } from './update-ticket/update-ticket.component';
+import { TicketDetailsComponent } from './ticket-details/ticket-details.component';
 
 const routes: Routes = [
   { path: '', component: ViewTicketsComponent },
+  { path: 'ticket-details', component: TicketDetailsComponent},
   { path: 'view-favorites', component: ViewFavoritesComponent },
   { path: 'add-ticket', component: AddTicketComponent },
   { path: 'update-ticket', component: UpdateTicketComponent },
